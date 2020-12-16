59aec43e71f6909189dc9f9a336417d407cb4776
MikChanna <channa.mik@gmail.com>
2020-12-16 12:49:35 -0500

    added title and style to navbar

---
+++ src/components/header/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+
+return (
+
+   <nav className= "navbar navbar-light bg-light">
+       <div className = "container-fluid"> 
+            <h1>Paymerang</h1>
+       </div>
+   </nav>
+
+)
+
+
+}
+
+export default Header;
