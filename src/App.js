adf0a7df1cf96f71f5b1f31a848dcc7eb8144a27
MikChanna <channa.mik@gmail.com>
2020-12-16 13:08:03 -0500

    added card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './App.css';
 import Header from "./components/header";
+import PayeeCard from "./components/payeeCard";
 
 function App() {
   return (
     <div>
       <Header/>
+      <PayeeCard/>
     </div>
   );
 }
